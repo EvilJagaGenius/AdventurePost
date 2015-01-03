fa11459bb4130c0eb07509103db60cdb0d6a35d9
Nathan @ Laptop <nathan.mcnany@gmail.com>
2015-01-03 14:07:10 -0500

    Working on a level editor

---
+++ Code/LevelEditor.py
@@ -0,0 +1,23 @@
+#APPLE level editor.
+#TTG
+
+import pygame, sys, os
+from pygame import *
+
+WX = 600
+WY = 400
+window = pygame.display.set_mode((WX, WY), 0, 32)
+
+def startScreen():
+    #1: Create new level
+    #2: Load old level
+    pass
+
+def loadLevel():
+    #Select .bmp file
+    #Select .txt file
+    pass
+
+def newLevel():
+    #Type in the level's name
+    pass
