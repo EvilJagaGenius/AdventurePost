7367978e03fcb7012895053f331aeedeef82d734
Nathan <delta-thunder@users.noreply.github.com>
2015-01-03 20:10:17 -0500

    Level Editor pt.2

--- Code/LevelEditor.py
+++ Code/LevelEditor.py
@@ -9,15 +9,53 @@ WY = 400
 window = pygame.display.set_mode((WX, WY), 0, 32)
 
 def startScreen():
-    #1: Create new level
-    #2: Load old level
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
     pass
 
 def loadLevel():
+    directory = os.path.join('..', 'Resources', 'APPLElvls')
+    bmpFiles = []
+    txtFiles = []
+    for i in os.listdir(directory):
+        if i.endswith('bmp'):
+            bmpFiles.append(i)
+        elif i.endswith('txt'):
+            txtFiles.append(i)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
     #Select .bmp file
     #Select .txt file
     pass
 
 def newLevel():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
     #Type in the level's name
     pass
+
+def edit():
+    while True:
+        for event in pygame.event.get:
+            if event.type == QUIT:
+                DTQuit()
+            if event.type == MOUSEBUTTONUP:
+                if event.button == 1:
+                    clicked = False
+                    releaseClick = True
+                if event.button == 4:
+                    wheelDown = True
+                if event.button == 5:
+                    wheelUp = True
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button = 1:
+                    clicked = True
